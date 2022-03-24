8650d651d43b4275e1fca0cb43b46b1184d5b0a9
Ben Podrazhansky <benpodrazhansky@gmail.com>
2022-03-24 18:53:49 -0400

    Made function for checking directories and copying their contents to other directories

--- USBChecker/checker.py
+++ USBChecker/checker.py
@@ -1,8 +1,23 @@
 import time
 import os
+import shutil
+def copyFilesToDirectory(dest, checkVariable, src='/media/pi'):
+    """
+    Args:
+        dest: The destination directory to copy the files to
+        checkVariable: A list variable with a single boolean in it. [True]. While the boolean inside the list is true,
+        the thread will continue checking the directory
+        src: The directory to be checking.
+    """
+    while checkVariable[0]:
+        contents = os.listdir(src)
+        for fName in contents:
+            path = src + "/" + fName
+            destPath = dest + "/" + fName
+            shutil.copy2(path, destPath)
+        time.sleep(2)
 
-while True:
-    #print("top of while loop")
-    time.sleep(5)
-    arr = os.listdir('/media/pi') 
-    print(arr)
+
+if __name__ == '__main__':
+    # To test the function
+    copyFilesToDirectory('temp', [True], src='tempSrc')
