245b3aafd13b888949c7c75ae32b4bd2057b7b95
Ben Podrazhansky <benpodrazhansky@gmail.com>
2022-04-23 19:25:00 -0400

    Added npm install yarn to install dependencies file

--- install_dependencies.py
+++ install_dependencies.py
@@ -5,4 +5,5 @@ if __name__ == '__main__':
     pip.main('install numpy Pillow opencv-python screeninfo'.split(" "))
     pip.main('install Flask Flask-RESTful Flask-Cors'.split(' '))
     os.chdir("virtual-mouthpiece")
+    os.system('npm install -g yarn')
     os.system("yarn install")
